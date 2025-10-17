1f2dade2093a7b6fb9c7fea3baabfc3c9bc4b92b
gabrgarc <gabrgarc@student.42sp.org.br>
2025-10-17 19:52:41 -0300

    refactored decimal for added function sign

--- ft_aux_nbrs.c
+++ ft_aux_nbrs.c
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 14:11:59 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/10/17 10:19:38 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2025/10/17 17:11:48 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ int	ft_aux_nbr(va_list ap, t_format *flags)
 	count = 0;
 	num = va_arg(ap, int);
 	result = ft_itoa(num);
-	if (((flags->flags & PLUS) || (flags->flags & SPACE)) && num > 0)
-		result = string_realloc(flags, result);
 	count = ft_strlen(result);
+	if (((flags->flags & PLUS) || (flags->flags & SPACE)) && num > 0)
+		count += sign(flags);
 	if (flags->width > count)
 		count += ft_pad(flags->width - count, flags);
 	ft_putstr_fd(result, 1);
@@ -57,17 +57,13 @@ int	ft_aux_hex(va_list ap, t_format *flags)
 		symbols = "0123456789ABCDEF";
 	result = ft_itoa_base(va_arg(ap, unsigned int), symbols);
 	count = ft_strlen(result);
+	if (flags->flags & HASHTAG)
+		count += prefix(flags);
 	if ((flags->flags & HASHTAG) && flags->width > count + 2)
-		count += ft_pad(flags->width - count - 2, flags);
+		count += ft_pad(flags->width - count, flags);
 	if (!(flags->flags & HASHTAG) && flags->width > count)
 		count += ft_pad((flags->width - count), flags);
-	if ((flags->flags & HASHTAG) && flags->specifier == 'x')
-		ft_putstr_fd("0x", 1);
-	if ((flags->flags & HASHTAG) && flags->specifier == 'X')
-		ft_putstr_fd("0X", 1);
 	ft_putstr_fd(result, 1);
-	if ((flags->flags & HASHTAG))
-		count += 2;
 	free(result);
 	return (count);
 }
