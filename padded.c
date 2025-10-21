ed8e37010e1cd4f4f530f2843958a8370b3d1bd2
gabrgarc <gabrgarc@student.42sp.org.br>
2025-10-20 21:02:41 -0300

    correct logic sign and add precision

--- padded.c
+++ padded.c
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 14:42:18 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/10/18 16:31:15 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2025/10/20 20:48:01 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,14 @@ int	ft_pad(int size, t_format *parameters)
 	return (printd);
 }
 
-int	sign(t_format *format)
+int	sign(t_format *format, int num)
 {
-	if (format->flags & PLUS)
+	if ((format->flags & PLUS) && num >= 0)
 		ft_putchar_fd('+', 1);
-	if (format->flags & SPACE)
+	if ((format->flags & SPACE) && num >= 0)
 		ft_putchar_fd(' ', 1);
+	if (num < 0)
+		ft_putchar_fd('-', 1);
 	return (1);
 }
 
@@ -44,4 +46,32 @@ int	prefix(t_format *format)
 	if (format->specifier == 'X')
 		ft_putstr_fd("0X", 1);
 	return (2);
+}
+
+char	*precision(t_format *format, char *num)
+{
+	int		len;
+	int		i;
+	int		j;
+	int		lending_zeros;
+	char	*new_str;
+
+	if (num[0] == '0' && format->precision == 0) // 0 print nothing
+	{
+		free(num);
+		new_str = ft_calloc(1, 1);
+		return (new_str);
+	}
+	len = ft_strlen(num);
+	lending_zeros = format->precision - len;
+	if (format->precision > len)
+		new_str = malloc(sizeof(char) * format->precision + 1);
+	i = 0;
+	while (lending_zeros - i)
+		new_str[i++] = '0';
+	j = 0;
+	while (len - j)
+		new_str[i++] = num[j++];
+	free(num);
+	return (new_str);
 }
