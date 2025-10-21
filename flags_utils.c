be172827b46f00a87b06928c769cdb60f62d71bd
gabrgarc <gabrgarc@student.42sp.org.br>
2025-10-20 21:04:44 -0300

    fix: obtaining value accuracy

--- flags_utils.c
+++ flags_utils.c
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 08:36:26 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/10/18 14:43:00 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2025/10/20 17:26:01 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	flag_width(t_format *parameters, char c)
 {
 	if (parameters->flags & DOT)
 	{
-		parameters->precision = (parameters->width * 10) + c - '0';
+		parameters->precision = (parameters->precision * 10) + c - '0';
 		return ;
 	}
 	parameters->width = (parameters->width * 10) + c - '0';
