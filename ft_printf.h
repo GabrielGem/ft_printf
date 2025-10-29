f1a2d648b2706958687f8d44dee6b8e2e90dacdc
gabrgarc <gabrgarc@student.42sp.org.br>
2025-10-29 16:02:08 -0300

    update prototype function sign

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 13:41:06 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/10/20 20:18:25 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2025/10/25 16:20:21 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,7 @@ void		flag_width(t_format *parameters, char c);
 void		switch_flags(t_format *format, char c);
 
 int			ft_pad(int size, t_format *parameters);
-int			sign(t_format *format, int num);
+char		sign(t_format *format, int num);
 int			prefix(t_format *format);
 char		*precision(t_format *format, char *num);
 
